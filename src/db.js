b85d232b0f8dc0eb1384d20e1997ca153cf4bb86
neleon10 <cvn45@hotmail.com>
2022-08-19 14:52:19 -0300

    deploy_branch_to_main

--- src/db.js
+++ src/db.js
@@ -4,12 +4,19 @@ const fs = require('fs');
 const path = require('path');
 const Medical_Record = require('./models/Medical_Record');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST
+  DATABASE_URL
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/medicineapp`, {
+const sequelize = new Sequelize(DATABASE_URL, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false,// lets Sequelize know we can use pg-native for ~30% more speed
+  //extra configuration for heroku
+  dialectOptions:{
+    ssl:{
+      require:true,
+      rejectUnauthorized:false
+    }
+  },
 });
 const basename = path.basename(__filename);
 
