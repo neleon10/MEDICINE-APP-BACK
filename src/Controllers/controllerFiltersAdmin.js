b7c09caac8c6e29c1afd3c8b1c6144151b31657b
ESMENIELD <esmeralda.nield@alu.ing.unlp.edu.ar>
2022-08-23 10:46:59 -0300

    routes admin finish

--- src/Controllers/controllerFiltersAdmin.js
+++ src/Controllers/controllerFiltersAdmin.js
@@ -4,94 +4,164 @@ const express = require("express");
 const {
   User,
   Professional,
-  Medicalrecord,
-  Comments,
   Appointment,
   Ad,
 } = require("../db");
 
 //get users
 const getFilterUsers = async (req, res, next) => {
-  const { ranking, appointment, createdProf, createdUser, active, noActive } =
-    req.query;
+  const {ranking, appointment, created, active, grafic } = req.query;
 
   try {
-    const allUsers = await User.findAll({ include: [{ model: Appointment, include: [Ad]}] });
+    const allAppointments = await Appointment.findAll({
+      where: { status: "pending" },
+      include: [Ad],
+    });
+    const allUsers = await User.findAll({
+      include: [{ model: Appointment, include: [Ad] }, { model: Professional }],
+    });
+    const UserLastedCreated = await User.findAll({
+      where: { rol: "user" },
+      order: [["createdAt", "DESC"]],
+    });
+    const professionalLastedCreated = await User.findAll({
+      where: { rol: "professional" },
+      order: [["createdAt", "DESC"]],
+    });
+    const bestProffesional = await User.findAll({
+      include: [{ model: Appointment, include: [Ad] }, { model: Professional }],
+      order: [[Professional, "ranking", "DESC"]],
+      where: { rol: "professional" },
+    });
+    const worseProfessionals = await User.findAll({
+      include: [{ model: Appointment, include: [Ad] }, { model: Professional }],
+      order: [[Professional, "ranking", "ASC"]],
+      where: { rol: "professional" },
+    });
     const allUsersUsers = allUsers.filter((user) => user.rol === "user");
-    const allUsersProfessionals = allUsers.filter(
-      (user) => user.rol === "professional"
-    );
+    const allProfessionals = allUsers.filter((user) => user.rol === "user");
 
-    if (appointment) {
-      const usersAppointmentsCompleted = allUsersUsers.filter((user) =>
-        user.appointments?.map(
-          (appointment) => appointment.status === "pending"
-        )
-      );
-      let infoUsers = [];
-      
-      usersAppointmentsCompleted.forEach((user) => {
-        let a = {};
-        a["userEmail"] = user.email;
-        a["totalAppointments"] = user.appointments.length;
-        a["totalPricesAppointments"] = 0;
-        user.appointments.map((ap) => {
-        console.log(ap.ad.price)
-          a.totalPricesAppointments += parseInt(ap.ad.price);
-        })
-        infoUsers.push(a);
-      })
-      res.status(200).send(infoUsers);
-    }
-    if (ranking) {
-      if(ranking==='mejores'){
-        allUsersProfessionals.sort((a, b) => {
-          if (a.ranking > b.ranking) return 1;
-          if (a.ranking < b.ranking) return -1;
-          return 0;
-        });
-      }else{
-        allUsersProfessionals.sort((a, b) => {
-          if (a.ranking > b.ranking) return 1;
-          if (a.ranking < b.ranking) return -1;
-          return 0;
-        });
-        
-      }
-      //devuelve los profesionales ordenados por mayor ranking
-      res.status(200).send(allUsersProfessionals);
-    }
-    // if (createdProf) {
-    //   allUsersProfessionals.sort((a, b) => {
-    //     if (a.createAt > b.createAt) return 1;
-    //     if (a.createAt < b.createAt) return -1;
-    //     return 0;
-    //   });
-    //   //devuelve los profesionales ordenados por ultimos creados
-    //   res.status(200).send(allUsersProfessionals);
-    // // }
-    // if (createdUser) {
-    //   allUsersUsers.sort((a, b) => {
-    //     if (a.createAt > b.createAt) return 1;
-    //     if (a.createAt < b.createAt) return -1;
-    //     return 0;
-    //   });
-    //   //devuelve los usuarios rol user ordenados por ultimos creados
-    //   res.status(200).send(allUsersUsers);
-    // }
-    if (active) {
-      allUsers.filter((user) => user.active === true);
-      res.status(200).send(allUsers);
-    }
-    if (noActive) {
-      allUsers.filter((user) => user.active === false);
-      res.status(200).send(allUsers);
-    }
     
+//****** respuestas segun query ******/ 
+
+//****APPOINTMENTS */
+    if (appointment && appointment !== "undefined") {
+      if(appointment==="users"){
+
+        const usersAppointmentsCompleted = allUsersUsers.filter((user) =>
+          user.appointments?.map(
+            (appointment) => appointment.status === "completed"
+          )
+        );
+        let infoUsers = [];
+  
+        usersAppointmentsCompleted.forEach((user) => {
+          let a = {};
+          a["userEmail"] = user.email;
+          a["userEmail"] = user.name;
+          a["totalAppointments"] = user.appointments.length;
+          a["totalPricesAppointments"] = 0;
+          a["active"] = user.active;
+  
+          user.appointments.map((ap) => {
+            console.log(ap.ad.price);
+            a.totalPricesAppointments += parseInt(ap.ad.price);
+          });
+  
+          infoUsers.push(a);
+        });
+        infoUsers.sort((a,b)=>{
+          if(a.totalAppointments<b.totalAppointments) return 1
+          if(a.totalAppointments>b.totalAppointments) return -1
+          return 0
+        })
+        res.status(200).send(infoUsers);
+      
+      // }else{
+      //   const professionalAppointmentsCompleted = allUsersUsers.filter((user) =>
+      //     user.appointments?.map(
+      //       (appointment) => appointment.status === "pending"
+      //     )
+      //   );
+      //   let infoProfessionals = [];
+  
+      //   professionalAppointmentsCompleted.forEach((user) => {
+      //     let a = {};
+      //     a["userEmail"] = user.email;
+      //     a["userEmail"] = user.name;
+      //     //a["medicalLicense"]=user.professional.medicalLicense
+      //     a["totalAppointments"] = user.appointments.length;
+      //     a["totalPricesAppointments"] = 0;
+      //     a["active"] = user.active;
+  
+      //     user.appointments.map((ap) => {
+      //       console.log(ap.ad.price);
+      //       a.totalPricesAppointments += parseInt(ap.ad.price);
+      //     });
+  
+      //     infoProfessionals.push(a);
+      //   });
+      //   res.status(200).send(professionalAppointmentsCompleted);
+
+      }
+    }
+
+    // ***  **RANKING******  
+
+
+    if (ranking && ranking !== "undefined") {
+      //devuelve los profesionales ordenados por mayor ranking
+      if (ranking === "mejores") {
+        res.status(200).send(bestProffesional);
+      } else {
+        res.status(200).send(worseProfessionals);
+      }
+    }
+///*******CREATED***** */
+    if (created && created !== "undefined") {
+      if (created === "users") {
+        res.status(200).send(UserLastedCreated);
+      } else {
+        res.status(200).send(professionalLastedCreated);
+      }
+    }
+
+ ///*****ACTIVE ****** */   
+
+    if (active && active !== "undefined") {
+      if (active === "yes") {
+        allUsers.filter((user) => user.active === true);
+        res.status(200).send(allUsers);
+      } else {
+        allUsers.filter((user) => user.active === false);
+        res.status(200).send(allUsers);
+      }
+    }
+
+  //*****GRAFIC*****/
+      if (grafic && grafic !== "undefined") {
+      let infroGraf = [];
+      if (grafic === "allAppoinments") {
+        for (let index = 0; index < 12; index++) {
+          console.log(index);
+          let a = {};
+          a["mes"] = `${index + 1}`;
+          a["totalPrice"] = 0;
+
+          allAppointments.forEach((appointment) => {
+            if (appointment.date[1] === `${index}`) {
+              a.totalPrice += parseInt(appointment.ad.price);
+            }
+          });
+          infroGraf.push(a);
+        }
+        res.status(200).send(infroGraf);
+      }
+    }
   } catch (error) {
     next(error);
   }
 };
-module.exports={
-  getFilterUsers
-}
+module.exports = {
+  getFilterUsers,
+};
